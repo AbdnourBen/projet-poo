62201b8ee32a9ffc1ca12f16e5f1059cf1cf5492
AbdnourBen <benmeridjaabdenour@gmail.com>
2024-12-29 20:39:10 +0100

    Create Property.java

---
+++ Property.java
@@ -0,0 +1,17 @@
+public class Property {
+    private String type;
+    private double size;
+    private double price;
+    private String location;
+    private String description;
+
+    public Property(String type, double size, double price, String location, String description) {
+        this.type = type;
+        this.size = size;
+        this.price = price;
+        this.location = location;
+        this.description = description;
+    }
+
+    // Getters and setters
+}
