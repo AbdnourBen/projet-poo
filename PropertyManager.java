e12605a4157c73e6207a293501a9111ce2ecd824
AbdnourBen <benmeridjaabdenour@gmail.com>
2024-12-29 20:42:06 +0100

    Create PropertyManager.java

---
+++ PropertyManager.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class PropertyManager {
+    private List<Property> properties = new ArrayList<>();
+
+    public void addProperty(Property property) {
+        properties.add(property);
+    }
+
+    // Other methods (modify, delete, search)
+}
