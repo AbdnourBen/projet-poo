a2750db8371815e5bd41138accc0b9827778afa1
AbdnourBen <benmeridjaabdenour@gmail.com>
2024-12-29 20:42:47 +0100

    Create ClientManager.java

---
+++ ClientManager.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class ClientManager {
+    private List<Client> clients = new ArrayList<>();
+
+    public void addClient(Client client) {
+        clients.add(client);
+    }
+
+    // Other methods (modify, delete)
+}
