62df3eeb408a3d545e84309d16484c4f42577f15
AbdnourBen <benmeridjaabdenour@gmail.com>
2024-12-29 20:39:47 +0100

    Create Client.java

---
+++ Client.java
@@ -0,0 +1,13 @@
+public class Client {
+    private String name;
+    private String contactInfo;
+    private String preferences;
+
+    public Client(String name, String contactInfo, String preferences) {
+        this.name = name;
+        this.contactInfo = contactInfo;
+        this.preferences = preferences;
+    }
+
+    // Getters and setters
+}
