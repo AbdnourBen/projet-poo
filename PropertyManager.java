471c26a7d06bd7f476be73fbb93eee36df9dd97e
AbdnourBen <benmeridjaabdenour@gmail.com>
2025-01-01 17:34:39 +0100

    Update PropertyManager.java

--- PropertyManager.java
+++ PropertyManager.java
@@ -8,5 +8,31 @@ public class PropertyManager {
         properties.add(property);
     }
 
-    // Other methods (modify, delete, search)
+
+    public void modifyProperty(int index, Property newProperty) {
+        if (index >= 0 && index < properties.size()) {
+            properties.set(index, newProperty);
+        } else {
+            System.out.println("Invalid index");
+        }
+    }
+
+    public void deleteProperty(int index) {
+        if (index >= 0 && index < properties.size()) {
+            properties.remove(index);
+        } else {
+            System.out.println("Invalid index");
+        }
+    }
+
+    public Property searchProperty(String propertyName) {
+        for (Property property : properties) {
+            if (property.getName().equals(propertyName)) {
+                return property;
+            }
+        }
+        return null; // Return null if not found
+    }
+}
+    /
 }
